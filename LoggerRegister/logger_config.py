c1c6fca4b549fbd6dca6d268cd7784951560bc2b
willy-hash <will.tech.code@gmail.com>
2025-01-06 20:47:23 -0500

    Features improving in package[LoggerRegister]; added to .gitignore files .log

--- LoggerRegister/logger_config.py
+++ LoggerRegister/logger_config.py
@@ -4,5 +4,3 @@ logging.basicConfig(filename="LoggerRegister/LoggerFiles/app.log",
                     level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
-
-#logger = logging.getLogger(__name__)
