2465ec75c8040c651ed255a717ad783a1853f472
willy-hash <will.tech.code@gmail.com>
2025-01-07 18:07:07 -0500

    Implement new feature in module[LoggerRegister.LoggerFiles.logger_config] Established StreamHandler

--- LoggerRegister/logger_config.py
+++ LoggerRegister/logger_config.py
@@ -1,32 +1,17 @@
 import os
 import logging
-from logging.handlers import RotatingFileHandler
 
-env = os.getenv('RENDER') == 'True'
-
-logging.basicConfig(level=logging.DEBUG if not env else logging.INFO,
+logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
 
 logger = logging.getLogger()
 
-if env:
-    server_handler = logging.StreamHandler()
-    server_handler.setLevel(logging.INFO)
-    server_handler.setFormatter(
-    logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(name)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S'))
+server_handler = logging.StreamHandler()
+server_handler.setLevel(logging.INFO)
+server_handler.setFormatter(
+logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(name)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S'))
 
-    logger.addHandler(server_handler)
-    logger.debug("Logger Configured for Production)")
+logger.addHandler(server_handler)
+logger.debug("Logger Configured for Production)")
 
-else:
-    local_handler = RotatingFileHandler(
-        filename="LoggerRegister/LoggerFiles/app.log",
-        maxBytes=100000,
-        backupCount=3
-    )
-    local_handler.setLevel(logging.DEBUG)
-    local_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
-
-    logger.addHandler(local_handler)
-    logger.debug("Logger Configured for Development)")
